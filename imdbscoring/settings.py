e22270ac1fdf0412b15a05a71ae39bcef322f21c
unknown <mahabubpro@outlook.com>
2019-11-06 02:34:31 +0530

    debug set to true

--- imdbscoring/settings.py
+++ imdbscoring/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'imdbscoring.wsgi.application'
 #     }
 # }
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
